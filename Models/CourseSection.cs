b2d94a031c6dcd15e2fc24b94cd03154e1b01c6c
jenasalim <jenamohsensalim022@gmail.com>
2026-08-29 14:54:47 +0300

    Add Authentication & Users module
    Register, Login, Logout, Cookie Authentication, Roles, User Management, Validation

---
+++ Models/CourseSection.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pathway.Models
+{
+    public class CourseSection
+    {
+        public int SectionId { get; set; }
+
+        [Required(ErrorMessage = "Section title is required")]
+        [StringLength(200, MinimumLength = 2,
+            ErrorMessage = "Section title must be between 2 and 200 characters")]
+        public string Title { get; set; }
+
+        [Required]
+        public int CourseId { get; set; }
+
+        [Range(1, int.MaxValue,
+            ErrorMessage = "Order must be greater than 0")]
+        public int Order { get; set; }
+
+        public Course Course { get; set; }
+
+        public ICollection<Lesson> Lessons { get; set; }
+    }
+}
